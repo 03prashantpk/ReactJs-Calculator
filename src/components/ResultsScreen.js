d096bb070393c509d5f2fd32c38b189fac248fc8
prashant kumar <prashantmanwan@gmail.com>
2023-04-01 22:37:50 +0530

    history and other components added

--- src/components/ResultsScreen.js
+++ src/components/ResultsScreen.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import Buttons from "./Buttons";
+import History from "./History";
+import { calculateResult } from "./logics";
 
 const ResultScreen = () => {
-  const [inputValue, setInputValue] = useState(0);
+  const [inputValue, setInputValue] = useState("0");
   const [isOn, setIsOn] = useState(false);
+  const [history, setHistory] = useState([]);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -17,60 +20,34 @@ const ResultScreen = () => {
   const handleButtonPress = (value) => {
     if (value === "clear") {
       setInputValue("0");
-        
     } else if (value === "=") {
       try {
         const result = calculateResult(inputValue);
         setInputValue(result);
+        setHistory((prevHistory) => [
+          ...prevHistory,
+          `${inputValue} = ${result}`,
+        ]);
       } catch (e) {
         setInputValue("Invalid Expression");
       }
     } else if (value === "+" || value === "-" || value === "*" || value === "/") {
-      setInputValue((prevValue) => String(prevValue) + String(value));
+      setInputValue((prevValue) => `${prevValue}${value}`);
     } else {
       if (inputValue === "0" && value !== ".") {
         setInputValue(value);
       } else {
-        setInputValue((prevValue) => String(prevValue) + String(value));
+        setInputValue((prevValue) => `${prevValue}${value}`);
       }
     }
   };
 
-  const calculateResult = (input) => {
-    let result = 0;
-    let operator = "+";
-    let numStr = "";
-
-    for (let i = 0; i < input.length; i++) {
-      const char = input.charAt(i);
-      if (/\d/.test(char) || char === ".") {
-        numStr += char;
-      } else if (/\+|-|\*|\//.test(char)) {
-        result = operate(operator, result, parseFloat(numStr));
-        operator = char;
-        numStr = "";
-      }
-    }
-
-    result = operate(operator, result, parseFloat(numStr));
-    return result.toString();
+  const handleEntryClick = (entry) => {
+    const index = entry.lastIndexOf("=");
+    const modifiedEntry = entry.substring(0, index).trim();
+    setInputValue(modifiedEntry);
   };
-
-  const operate = (operator, num1, num2) => {
-    switch (operator) {
-      case "+":
-        return num1 + num2;
-      case "-":
-        return num1 - num2;
-      case "*":
-        return num1 * num2;
-      case "/":
-        return num1 / num2;
-      default:
-        return 0;
-    }
-  };
-
+  
   return (
     <>
       <p>{isOn ? "Power On" : "Power Off"}</p>
@@ -87,6 +64,11 @@ const ResultScreen = () => {
         onClick={handleToggle}
       />
       <Buttons handleButtonPress={handleButtonPress} isOn={isOn} />
+      <History
+        entries={history}
+        isOn={isOn}
+        handleEntryClick={handleEntryClick}
+      />
     </>
   );
 };
